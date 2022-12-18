fc8b10f46c6579ed9ba16c44d006fdc4b98c3531
CECurtiss <curtissclinton@gmail.com>
2022-12-18 16:05:01 -0600

    fixed problem in server.js that was preventing data persisting

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-// force used to destroy old tables of same name and create new one with seeds. Dev use only. NOT IN PRODUCTION.
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
